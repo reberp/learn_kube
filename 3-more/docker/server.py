d91af42b4689fce97506c3bf0df8ef4f8d65f411
pat <pat>
2019-12-21 10:22:24 -0800

    added secrets and parametrized configmap and secrets from values, eventually env var the secrets themselves or vault

--- 3-more/docker/server.py
+++ 3-more/docker/server.py
@@ -1,7 +1,15 @@
 from flask import Flask
 from flask import request
+from flask_mysqldb import MySQL
+
 app = Flask(__name__)
 
+#can get service address with $RELEASE-NAME{and then whatever}
+app.config['MYSQL_HOST'] = '$RELEASE-NAME-service'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'root'
+app.config['MYSQL_DB'] = 'MyDB'
+mysql = MySQL(app)
 
 @app.route('/')
 def hello_world():
@@ -14,6 +22,9 @@ def shutdown():
         raise RuntimeError('Not running with the Werkzeug Server')
     func()
 
+@app.route('/connect', methods=['GET'])
+def connect():
+    pass
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
